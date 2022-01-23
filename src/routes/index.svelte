39757c680e21aee926acc46e48c02edb21987f2a
matskevichAnthony <91896307+matskevichAnthony@users.noreply.github.com>
2022-01-23 13:56:52 +0000

    API request, initial GRID styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`  https://api.themoviedb.org/3/movie/popular?api_key=5baad519e3064eb2f2026518e5cf96b2&language=en-US&page=1`
+		);
+		const data = await res.json();
+		console.log(data);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/popularMovies.svelte';
+	export let popular;
+</script>
+
+<section><PopularMovies {popular} /></section>
